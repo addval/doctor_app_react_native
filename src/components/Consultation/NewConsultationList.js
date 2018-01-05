9468df3fd8fb134e73937f572d59d86dc5fe917f
Robin Garg <robingarg@Robins-MacBook-Pro.local>
2018-01-05 12:38:58 +0530

    empty component created for inbox

---
+++ src/components/Consultation/NewConsultationList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import {
+    View, 
+    Text
+} from 'react-native'
+
+export default class NewConsultationList extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={{fontSize:30}}>New Cosultation</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgrondColor: 'lightgray',
+    },
+})
